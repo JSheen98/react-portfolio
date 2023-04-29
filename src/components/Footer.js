ae22f1a8908b71e55479f02ac76c8655973984dd
Jsheen98 <jsheendev@gmail.com>
2023-04-29 09:04:31 -0600

    Added footer links, other small bug fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React from "react"
+import './styles/Navbar.css'
 
+const styles = {
+  footer: {
+    left: '0',
+    bottom: '0',
+    width: '100%',
+    background: '#5BADBF',
+    textAlign: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  anchors: {
+    textDecoration: 'none',
+    color: '#B9FAF8',
+    fontSize: '25px'
+  },
+  style: {
+    clear: 'both'
+  }
+}
 
 export default function Footer() {
-    return (
-      <h1>This is a footer!</h1>
-    );
-  }
+  return (
+    <div style={styles.style}>
+    <section className="justify-content-center d-flex" style={styles.footer}>
+      <ul className="list">
+        <li className="items">
+          <a style={styles.anchors} href="https://twitter.com/jsheendev">Twitter</a>
+        </li>
+        <li className="items">
+          <a style={styles.anchors} href="https://www.linkedin.com/in/jackson-sheen-1b50a2258/">LinkedIn</a>
+        </li>
+        <li className="items">
+          <a style={styles.anchors} href="https://docs.google.com/document/d/15mmJ_caZAD4yJgpKUq8CeohSo5KBZx3H1iuN-qyJ3Os/edit?usp=sharing">Resume</a>
+        </li>
+      </ul>
+    </section>
+    </div>
+  );
+}
--- src/components/tabs/Home.js
+++ src/components/tabs/Home.js
@@ -6,18 +6,38 @@ const styles = {
         background: '#5D5E60'
     },
     cardWidth: {
-        width: '18rem'
+        width: '60rem'
     }
 }
 
 export default function Home() {
     return (
-        <section style={styles.body}>
-            <div className="card" style={styles.cardWidth}>
+        <section className="d-flex justify-content-center" style={styles.body}>
+            <div className="card ms-4 me-4 mb-4" style={styles.cardWidth}>
                 <div className="card-body">
                     <h5 className="card-title">About me</h5>
                     <h6 className="card-subtitle mb-2 text-muted">Jackson Sheen</h6>
                     <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <br></br>
+                    <p className="card-text">Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad 
+                    minim veniam, quis nostrud exercitation ullamco laboris 
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                    dolor in reprehenderit in voluptate velit esse cillum dolore
+                     eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                      non proident, sunt in culpa qui officia deserunt mollit 
+                      anim id est laborum.</p>
+                      <br></br>
+                    <p className="card-text">Lorem ipsum dolor sit amet, 
+                    consectetur adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad 
+                    minim veniam, quis nostrud exercitation ullamco laboris 
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                    dolor in reprehenderit in voluptate velit esse cillum dolore
+                     eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                      non proident, sunt in culpa qui officia deserunt mollit 
+                      anim id est laborum.</p>      
                 </div>
             </div>
         </section>
